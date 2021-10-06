e871a119d92ef21f3ab29fe42cb69373f78e0b87
chanmin07 <cksals2589@naver.com>
2021-10-06 23:04:55 +0900

    Lv1_left/right

--- Lv1_K.java
+++ Lv1_K.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-
 public class Lv1_K {
     class Solution {
         public int[] solution(int[] array, int[][] commands) {
@@ -20,4 +19,24 @@ public class Lv1_K {
             return answer;
         }
     }
+    public static void main(String[] args) {
+        
+    }
 }
+
+/* 다른 사람 풀이
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i=0; i<commands.length; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
+        }
+
+        return answer;
+    }
+}
+*/
