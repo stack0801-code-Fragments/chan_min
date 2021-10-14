499a380b6c3b647f8b444bfc89fb73ff2233ecfd
chanmin07 <cksals2589@naver.com>
2021-10-14 13:21:54 +0900

    a

---
+++ Lv1_strMy.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Lv1_strMy {
+    class Solution {
+        public String[] solution(String[] strings, int n) {
+            String[] answer = new String[strings.length];
+            
+            // ArrayList 사용
+            ArrayList<String> array = new ArrayList<String>();
+                
+            for (int i=0; i<strings.length; i++) {
+                array.add(strings[i].charAt(n) + strings[i]);
+            }
+            
+            Collections.sort(array);
+            
+            for (int i=0; i<array.size(); i++) {
+                answer[i] = array.get(i).substring(1);
+            }
+            
+            return answer;
+        }
+    }
+}
