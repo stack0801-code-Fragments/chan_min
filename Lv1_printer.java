16ede9806ec34764287d5b910f4fa7a41f133f6f
chanmin07 <cksals2589@naver.com>
2021-11-19 15:14:38 +0900

    A

--- Lv1_printer.java
+++ Lv1_printer.java
@@ -1,3 +1,28 @@
+class Solution {
+    public int solution(int[] priorities, int location) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int answer = 0;
+        
+        for (int i = 0; i < priorities.length; i++) {
+            pq.add(priorities[i]);
+        }
+        
+        while (!pq.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (priorities[i] == pq.peek()) {
+                    if (i == location) {
+                        answer++;
+                        return answer;
+                    }
+                    pq.poll();
+                    answer++;
+                }
+            }
+        }
+        return -1;
+    }
+}
+
 public class Lv1_printer {
     public static void main(String[] args) {
         
