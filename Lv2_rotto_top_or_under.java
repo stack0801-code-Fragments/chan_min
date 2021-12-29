be055ee464cf9711e26d909a4869aee810e45524
chanmin07 <cksals2589@naver.com>
2021-12-29 22:45:46 +0900

    A

---
+++ Lv2_rotto_top_or_under.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = {};
+
+        
+        return answer;
+    }
+}
+
+public class Lv2_rotto_top_or_under {
+    
+}
