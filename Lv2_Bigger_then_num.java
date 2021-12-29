d5fe0bd4bee4ec81287199b120ddc6f75b98dee6
chanmin07 <cksals2589@naver.com>
2021-12-29 22:46:51 +0900

    a

--- Lv2_Bigger_then_num.java
+++ Lv2_Bigger_then_num.java
@@ -3,16 +3,12 @@ import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
         String answer = "";
-        // int -> String 배열로
-        ArrayList<String> strNumbers = new ArrayList();
+        ArrayList<String> strNumbers = new ArrayList(); // int -> String 배열변경
         for(Integer num : numbers){
             strNumbers.add(String.valueOf(num));
-        }
-        // 내림차순 정렬
-        Collections.sort(strNumbers, (a, b) -> (b + a).compareTo(a + b));
-        // numbers가 [0,0,0] 등으로 주어지는 경우는 "000"이 아닌 "0" 리턴
-        if(strNumbers.get(0).startsWith("0")) return "0";
-        // answer에 정렬된 것을 담기
+        }// 내림차순 정렬
+        Collections.sort(strNumbers, (a, b) -> (b + a).compareTo(a + b));// 0이 중복이 되면 0 하나만 리턴
+        if(strNumbers.get(0).startsWith("0")) return "0";// answer에 정렬된 것을 넣음
         for(String s : strNumbers){
             answer += s;
         }
