401a75b539f21115b45e99ca48aafc4713faaf9a
chanmin07 <cksals2589@naver.com>
2021-12-30 13:55:03 +0900

    a

--- Lv2_Bigger_then_num.java
+++ Lv2_Bigger_then_num.java
@@ -3,13 +3,13 @@ import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
         String answer = "";
-        ArrayList<String> strNumbers = new ArrayList(); // int -> String 배열변경
+        ArrayList<String> strNum = new ArrayList(); // int -> String 배열변경
         for(Integer num : numbers){
-            strNumbers.add(String.valueOf(num));
+            strNum.add(String.valueOf(num));
         }// 내림차순 정렬
-        Collections.sort(strNumbers, (a, b) -> (b + a).compareTo(a + b));// 0이 중복이 되면 0 하나만 리턴
-        if(strNumbers.get(0).startsWith("0")) return "0";// answer에 정렬된 것을 넣음
-        for(String s : strNumbers){
+        Collections.sort(strNum, (a, b) -> (b + a).compareTo(a + b));// 0이 중복이 되면 0 하나만 리턴
+        if(strNum.get(0).startsWith("0")) return "0";// answer에 정렬된 것을 넣음
+        for(String s : strNum){
             answer += s;
         }
         return answer;
--- Lv2_rotto_top_or_under.java
+++ Lv2_rotto_top_or_under.java
@@ -1,8 +1,34 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = {};
+        int[] answer = {0, 0};
+        int zeroCnt = 0;
 
-        
+        Arrays.sort(lottos);
+        for(int i = 0; i < 6; i++) {
+            if(lottos[i] != 0) break;
+            zeroCnt++;
+        }
+
+        Arrays.sort(win_nums);
+        for(int j = 0; j < 6; j++) {
+            for(int k = zeroCnt; k < 6; k++) {
+                if(win_nums[j] != lottos[k])    continue;
+                answer[0]++;
+                answer[1]++;
+            }
+        }
+        answer[0] += zeroCnt;
+
+        for(int i = 0; i < 2; i++) {
+            if(answer[i] == 6) answer[i] = 1;
+            else if(answer[i] == 5) answer[i] = 2;
+            else if(answer[i] == 4) answer[i] = 3;
+            else if(answer[i] == 3) answer[i] = 4;
+            else if(answer[i] == 2) answer[i] = 5;
+            else answer[i] = 6;
+        }
         return answer;
     }
 }
@@ -10,3 +36,18 @@ class Solution {
 public class Lv2_rotto_top_or_under {
     
 }
+/*
+import java.util.Arrays;
+import java.util.stream.LongStream;
+
+class Solution {
+    public int[] solution(int[] lottos, int[] winNums) {
+        return LongStream.of(
+                (lottos.length + 1) - Arrays.stream(lottos).filter(l -> Arrays.stream(winNums).anyMatch(w -> w == l) || l == 0).count(),
+                (lottos.length + 1) - Arrays.stream(lottos).filter(l -> Arrays.stream(winNums).anyMatch(w -> w == l)).count()
+        )
+                .mapToInt(op -> (int) (op > 6 ? op - 1 : op))
+                .toArray();
+    }
+}
+*/
