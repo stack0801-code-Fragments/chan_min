0c084b1f896c3543a277d894667992052a7a238a
chanmin07 <cksals2589@naver.com>
2022-01-13 12:57:18 +0900

    a

--- Lv1_소수찾기.java
+++ Lv1_소수찾기.java
@@ -2,14 +2,14 @@ class Solution {
     public int solution(int n) { 
         int answer = 0; 
         for(int i = 2; i <= n; i++){ 
-            boolean chk = true; 
-            for(int j = 2; j*j <= i; j++){
-                 //배수 지우기 
+            boolean b = true; 
+            for(int j = 2; j*j <= i; j++){ //배수 지우기 
                  if(i % j == 0){ 
-                     chk = false; break; 
+                     b = false; 
+                     break; 
                     } 
                 } 
-                if(chk){ 
+                if(b){ 
                     answer++; 
                 } 
             } 
