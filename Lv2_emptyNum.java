845b3dbb3955f4d62d6cc7e032690dd2b6ce488c
chanmin07 <cksals2589@naver.com>
2022-01-13 12:39:38 +0900

    a

---
+++ Lv2_emptyNum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45; 
+        for(int number : numbers) {
+            answer -= number;
+        }    
+        return answer;
+    }
+}
+
+public static void main(String[] args){
+    void main(){
+
+    }
+}
