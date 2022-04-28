89ba27d0dc51f39c165abd21ea63716f9f98a4f7
night_kim <cksals2589@naver.com>
2022-04-28 18:36:36 +0900

    a

---
+++ 파이썬/문자열압축.py
@@ -0,0 +1,22 @@
+def solution(s):
+    answer = len(s)
+    # 1개 단위부터 압축 단위를 늘려가며 확인
+    for i in range(1, len(s) // 2 + 1):
+        comp = ""
+        prev = s[0:i]  # 앞에서부터 i만큼의 문자열 추출
+        count = 1
+    # 단위 크기만큼 증가시키며 이전 문자열과 비교
+        for j in range(i, len(s), i): 
+        # 이전 상태와 동일하다면 압축 횟수(count) 증가
+            if prev == s[j:j + i]:
+                count += 1    
+        # 다른 문자열이 나왔다면
+            else:
+                comp += str(count) + prev if count >= 2 else prev
+                prev = s[j:j + i]  # 다시 초기화
+                count = 1    
+        # 남아 있는 문자열에 대해서 처리
+        comp += str(count) + prev if count >= 2 else prev
+        # 만들어지는 문자열이 가장 짧은 것이 정답
+        answer = min(answer, len(comp))
+    return answer
