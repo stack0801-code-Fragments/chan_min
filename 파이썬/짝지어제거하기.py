328489e3492c14ff9290d9edc9437896cdd7220f
night_kim <cksals2589@naver.com>
2022-05-18 02:09:25 +0900

    a

--- 파이썬/짝지어제거하기.py
+++ 파이썬/짝지어제거하기.py
@@ -1,13 +1,18 @@
-def solution(s): 
-    stack = []
-    for i in s:
-        if len(stack) == 0: 
-            stack.append(i)
-        elif stack[-1] == i: 
-            stack.pop()
-        else: 
-            stack.append(i)
-    if len(stack) == 0: 
-        return 1
+def solution(s):
+    answer = []
+    
+    for i in range(len(s)):
+        if not answer: #stack 없음? -> 첫번째 비교를 위한 한개 넣기
+            answer.append(s[i])
+        else:
+            if s[i] == answer[-1]: #제일 뒤스텍이랑 비교
+                answer.pop() #맞으면 제일뒤 나가
+                print(answer)
+            else:
+                answer.append(s[i]) #아니면 i번째 뒤로 붙어
+                print(answer)
+    
+    if answer: 
+        return 0 #남음
     else: 
-        return 0 
+        return 1 #안남음
--- 파이썬/타겟넘버.py
+++ 파이썬/타겟넘버.py
@@ -1,23 +1,9 @@
 def solution(numbers, target):
-    answer = 0
-    n = len(numbers)
-    queue = [[numbers[0],0], [-1*numbers[0],0]]
-    
-    while queue:
-        temp, idx = queue.pop()
-        idx += 1
-        if idx < n :
-            queue.append([temp + numbers[idx], idx])
-            queue.append([temp - numbers[idx], idx])
-        else :
-            if temp == target :
-                answer += 1
-    return answer
-
-#def solution(numbers, target): 
-    if not numbers and target == 0 :
-        return 1
-    elif not numbers:
-        return 0
-    else:
-        return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
+    answer= [0]
+    for i in numbers: #넘버스 길이만큼
+        bfs_stack = [] #BFS 저장
+        for j in answer : #j를 i의 0값만큼 빙글빙글
+            bfs_stack.append(j+i) #0부터 numbers만큼 i를 더함
+            bfs_stack.append(j-i) #0부터 numbers만큼 i를 뺌
+        answer = bfs_stack #bfs값을 answer로 옴김
+    return answer.count(target) #타겟넘버만 카운트함
