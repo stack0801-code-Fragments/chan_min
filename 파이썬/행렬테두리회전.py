b837bb4eacd0b18930dc4c8d50c9a344ae78c904
night_kim <cksals2589@naver.com>
2022-05-19 04:37:20 +0900

    a

--- 파이썬/행렬테두리회전.py
+++ 파이썬/행렬테두리회전.py
@@ -1,3 +1,36 @@
 def solution(rows, columns, queries):
-    answer = 0
+    answer = []
+    circle = []
+    for i in range(rows): #1~36까지의 표 만들기
+        circle.append([x for x in range(i*columns+1, (i+1) * columns + 1)])
+        
+    for q in queries:
+        q = [x-1 for x in q]# 0부터 1씩 빼주는 값
+
+        arr = circle[q[0]][q[1]] # 좌측 상단값 저장
+        q_min = arr
+        
+        for i in range(q[0]+1, q[2]+1): #왼쪽 탐색
+            circle[i-1][q[1]] = circle[i][q[1]]
+            q_min = min(q_min, circle[i][q[1]])
+
+        for i in range(q[1] + 1, q[3] + 1): #아래쪽 탐색
+            circle[q[2]][i-1] = circle[q[2]][i]
+            q_min = min(q_min, circle[q[2]][i])
+        
+        for i in range(q[2] - 1, q[0] - 1, -1): #오른쪽 탐색
+            circle[i+1][q[3]] = circle[i][q[3]]
+            q_min = min(q_min, circle[i][q[3]])
+        
+        for i in range(q[3] - 1, q[1] - 1, -1): #위쪽 탐색
+            circle[q[0]][i+1] = circle[q[0]][i]
+            q_min = min(q_min, circle[q[0]][i])
+        
+        circle[q[0]][q[1]+1] = arr
+
+        answer.append(q_min)
+
     return answer
+    
+
+print(solution(6, 6, [[2,2,5,4],[3,3,6,6],[5,1,6,3]]))
