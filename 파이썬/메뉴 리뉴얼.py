fda7dfda4fde46673d1b3019ba77e52343c7dc67
night_kim <cksals2589@naver.com>
2022-05-20 04:20:30 +0900

    a

--- 파이썬/메뉴 리뉴얼.py
+++ 파이썬/메뉴 리뉴얼.py
@@ -1,3 +1,22 @@
-def solution(orders, course):
-    answer = 0
-    return answer
+import itertools #combinations는 하나의 리스트 내에서 모든 조합을 구하는 라이브러리
+import collections #Counter를 통해 리스트 내의 중복값을 모두 카운트한다
+
+def solution(orders, course): #course만큼 반복 ex)[2,3,4]->2개 조합 3개조합 4개조합같은 형식
+    answer = []
+    for i in course:
+        order_backup = []
+        for j in orders: #오더 수만큼 반복
+            com = itertools.combinations(sorted(j), i) #정렬하면서 오더 수만큼 코스 조합
+            print("재조합 : ", com)
+            order_backup += com
+        cnt = collections.Counter(order_backup) #조합수 카운트
+        print("카운터 : ", cnt)
+        if len(cnt) != 0 and max(cnt.values()) != 1: #조합 안나오거나 주문한 사람이 한명 뿐이면 무시
+            answer += [''.join(k) for k in cnt if cnt[k] == max(cnt.values())] #가장 많이 주문한거 추가
+        
+    print("추가할 코스 : ", answer)
+
+    return sorted(answer)
+
+
+solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4])
--- 파이썬/행렬테두리회전.py
+++ 파이썬/행렬테두리회전.py
@@ -5,26 +5,31 @@ def solution(rows, columns, queries):
         circle.append([x for x in range(i*columns+1, (i+1) * columns + 1)])
         
     for q in queries:
-        q = [x-1 for x in q]# 0부터 1씩 빼주는 값
+        q = [i-1 for i in q]# 0부터 1씩 빼주는 값
 
         arr = circle[q[0]][q[1]] # 좌측 상단값 저장
+        print("저장값 : ", arr)
         q_min = arr
         
         for i in range(q[0]+1, q[2]+1): #왼쪽 탐색
             circle[i-1][q[1]] = circle[i][q[1]]
-            q_min = min(q_min, circle[i][q[1]])
+            q_min = min(q_min, circle[i][q[1]]) #자리 바꿀때마다 작은거 비교
+            print("왼쪽 탐색 중 작은 값 : ", q_min)
 
         for i in range(q[1] + 1, q[3] + 1): #아래쪽 탐색
             circle[q[2]][i-1] = circle[q[2]][i]
-            q_min = min(q_min, circle[q[2]][i])
+            q_min = min(q_min, circle[q[2]][i]) #자리 바꿀때마다 작은거 비교
+            print("아래쪽 탐색 중 작은 값 : ", q_min)
         
         for i in range(q[2] - 1, q[0] - 1, -1): #오른쪽 탐색
             circle[i+1][q[3]] = circle[i][q[3]]
-            q_min = min(q_min, circle[i][q[3]])
+            q_min = min(q_min, circle[i][q[3]]) #자리 바꿀때마다 작은거 비교
+            print("오른쪽 탐색 중 작은 값 : ", q_min)
         
         for i in range(q[3] - 1, q[1] - 1, -1): #위쪽 탐색
             circle[q[0]][i+1] = circle[q[0]][i]
-            q_min = min(q_min, circle[q[0]][i])
+            q_min = min(q_min, circle[q[0]][i]) #자리 바꿀때마다 작은거 비교
+            print("위쪽 탐색 중 작은 값 : ", q_min)
         
         circle[q[0]][q[1]+1] = arr
 
