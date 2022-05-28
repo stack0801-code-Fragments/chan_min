f5b6b3584ae8b4a92a17603b913a691e65a253d9
night_kim <cksals2589@naver.com>
2022-05-29 04:47:55 +0900

    a

--- 파이썬/N으로표현.py
+++ 파이썬/N으로표현.py
@@ -1,5 +1,29 @@
 def solution(N, number):
-    answer = []
-    return answer
+    answer = [[N]]
+    
+    if N == number:
+        return 1
+
+    for i in range(2, 9):
+        for j in range(1, i // 2 + 1):  # N으로 나오는 조합
+            N_stack = []
+            for k in answer[j - 1]:
+                for p in answer[i - j - 1]:
+                    N_stack.append(k + p)
+                    N_stack.append(k - p)
+                    N_stack.append(p - k)
+                    N_stack.append(p * k)
+                    if k != 0:
+                        N_stack.append(p / k)
+                    if p != 0:
+                        N_stack.append(k / p)
+            N_stack.append(int(str(N) * i))
+            N_stack = list(set(N_stack))
+            if number in N_stack:
+                return i
+            answer.append(N_stack)
+    return -1
 
 print(solution(5, 12))
+
+#5번 예제가 안됨 실패한거임
