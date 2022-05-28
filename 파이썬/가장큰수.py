ade742cd7db20467f6f14c20e430e4f20338543b
night_kim <cksals2589@naver.com>
2022-05-29 04:25:27 +0900

    a

--- 파이썬/가장큰수.py
+++ 파이썬/가장큰수.py
@@ -1,7 +1,7 @@
 def solution(numbers):
     answer = ""
     numbers = list(map(str, numbers)) #문자열로 바꿈
-    sort_num = sorted(numbers, key = lambda x : x*5, reverse= True)
+    sort_num = sorted(numbers, key = lambda x : x*3, reverse= True)
     #내일차순 정렬을 하는데 각 키값을 3번씩 반복해서 값을 설정해서 한다 
     #[3, 30, 34, 5, 9] -> 역정렬 -> [9,5,34,30,3] -> 3배씩 -> [999,555,343434,303030,333]
     #이게 문자열 시점에서 보면 첫번째거 비교하고 두번째꺼를 비교하는데 0~1000까지만 하기때문에 자릿수 넘는건 무시됨
